d28bdcdd642856586fec79049a19eb7f1c8515cd
Abdullah Nadeem <abdnadeem382@gmail.com>
2020-05-11 16:42:31 +0500

    Added Finish Button

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import {Button,FAB } from 'react-native-paper';
 import data from './DummyData';
 
-const shuffled = data.sort(() => 0.5 - Math.random());
-const randomQuestions = shuffled.slice(0, 5);
+let shuffled = [];
+let randomQuestions = [];
 
 export default App = () => {
   const [started, setStarted] = useState(false);
@@ -12,13 +12,20 @@ export default App = () => {
   const [score, setScore] = useState(0);
   const [answered, setAnswered] = useState(false);
   const [correct, setCorrect] = useState(4);
+  const [questions, setQuestions] = useState([])
+  
 
  
   showStart = () =>{
     return(
       <View style = {styles.container}>
         <Text style={styles.welcome}>WELCOME TO YOUR QUIZ</Text>
-        <Button mode ="contained" theme={theme} style={styles.Startbutton} onPress= {()=>setStarted(true)}>
+        <Button mode ="contained" theme={theme} style={styles.Startbutton} 
+        onPress= {()=>{setStarted(true)
+        shuffled = data.sort(() => 0.5 - Math.random());
+        randomQuestions = shuffled.slice(0, 5);
+        }
+        }>
           Let's Go
         </Button>
       </View>
@@ -38,6 +45,34 @@ export default App = () => {
   }
   
   showApp = () =>{
+    const fab = ( <FAB
+      theme={theme}
+      style={styles.fab}
+      icon="forward"
+      disabled ={answered ? false: true}
+      onPress={() => {if(questionNumber <randomQuestions.length-1){
+                      setQuestionNumber((questionNumber)=>questionNumber+1)
+                      setCorrect(4)}
+                      setAnswered(false)
+                    }}
+    />)
+    const finish = (
+      <Button
+        mode = "contained"
+        style = {styles.options}
+        theme= {finsihTheme}
+        onPress= {()=>{setQuestionNumber(0);
+          setScore(0);
+          setCorrect(4)
+          setStarted(false);
+          setAnswered(false)
+         }
+        }
+        >
+        FINISH
+      </Button>
+    )
+
     return (
       <View style = {styles.container}>
         <View style = {styles.header}>
@@ -54,24 +89,8 @@ export default App = () => {
             </Button>
           )
         })}
-
-        <FAB
-          theme={theme}
-          style={styles.fab}
-          icon="forward"
-          disabled ={answered ? false: true}
-          onPress={() => {if(questionNumber <randomQuestions.length-1){
-                          setQuestionNumber((questionNumber)=>questionNumber+1)
-                          setCorrect(4)}
-                          else{
-                            setQuestionNumber(0);
-                            setScore(0);
-                            setCorrect(4)
-                            setStarted(false);
-                          }
-                          setAnswered(false)
-                        }}
-        />
+        {(questionNumber === 4) ? finish : fab}
+       
       </View>
       
     )
@@ -86,6 +105,11 @@ export default App = () => {
 const theme = {
   colors:{
     primary: "#8de3d3"
+  }
+}
+const finsihTheme ={
+  colors:{
+    primary:"#4f8065"
   }
 }
 const Redtheme = {
